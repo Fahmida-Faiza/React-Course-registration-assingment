aee1e3749c9e1eea7ad4ef42f7811289c9254dba
Fahmida Faiza <fahmidafaiza57@gmail.com>
2023-09-14 21:35:25 +0600

    added price

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,11 +1,29 @@
 import PropTypes from 'prop-types';
 
 const Course = ({course}) => {
-    const{course_name}=  course;
+
+
+    const{course_name , image , course_details, Price , credit}=  course;
     return (
-        <div>
+        <div className=''>
+            <img src={image} alt={`image of the title ${course_name}`}/>
             <h2 className="text-3xl">{course_name}</h2>
-        </div>
+            <h3 className='text-2xl'>{course_details}</h3>
+            
+        <div className='flex justify-between'>
+            <div>
+{/* <img src="" alt="" /> */}
+<span>Price: {Price}</span>
+</div>
+<div>
+    <span>{credit}hr</span>
+
+</div>
+
+            </div>
+
+            </div>
+        
     );
 };
 
